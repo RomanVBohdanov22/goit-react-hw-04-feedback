389b0b1efe80baf0633be59b27e9599e11c20311
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-05-01 10:22:44 +0300

    Finally done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Statistics from './statistics';
 import FeedbackOptions from './feedbackoptions';
 import Notification from './notification';
 import Section from './section';
-import { useState, useEffect } from "react";
+import { useState } from 'react';
 
 const appStyles = {
   height: '100vh',
@@ -15,42 +15,59 @@ const appStyles = {
   color: '#010101',
 };
 
-let summ = 0;
-
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  useEffect(() => {
-    summ = good+neutral+bad;
-    console.log(summ);
-  }, [good, neutral, bad]);
+  const onLeaveFeedback = option => {
+    //console.log(option);
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        break;
+    }
+  };
 
+  const countTotalFeedback = () => {
+    return good + bad + neutral;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100) || 0;
+  };
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
   return (
-    <div
-      style={appStyles}
-    >
+    <div style={appStyles}>
       <Section title="Please leave feedback">
-        <button onClick={() => setGood(good + 1)}>Good: {good}</button>
-        <button onClick={() => setNeutral(neutral + 1)}>Neutral: {neutral}</button>
-        <button onClick={() => setBad(bad + 1)}>Bad: {bad}</button>
-       </Section>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
       React homework goit-react-hw-04-feedback
       <Section title="Statistics">
-        { summ ?
-          (<Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={summ}
-                positivePercentage={good/neutral}
-              />):(
-            <>
-                <Notification message="There is no feedback" />
-            </>
-          )
-        }
+        {total ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <>
+            <Notification message="There is no feedback" />
+          </>
+        )}
       </Section>
     </div>
   );
--- src/components/feedbackoptions/FeedbackOptions.jsx
+++ src/components/feedbackoptions/FeedbackOptions.jsx
@@ -4,23 +4,27 @@ import '../feedbackoptions/FeedbackOptions.css';
 //FeedbackOptions
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (<div className="feedback">        
-        {
-            options.map((option) => {
-    return (
-      <button type="button" name="option" key={option} onClick={() => onLeaveFeedback(option)}>
-        {option}
-      </button>
-    ); })
-        }
-    </div>);
+  return (
+    <div className="feedback">
+      {options.map(option => {
+        return (
+          <button
+            type="button"
+            name="option"
+            key={option}
+            onClick={() => onLeaveFeedback(option)}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FeedbackOptions;
 
-
-FeedbackOptions.propTypes= { 
-    options: PropTypes.array.isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
 };
-
